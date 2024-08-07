33f83332854d3e58edc5fa235e21b67c10caad9b
gosleii <sleii@inbox.ru>
2024-08-07 18:52:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+
+bot = telebot.TeleBot('7352707059:AAHGpbbY0nlfFMPNXJy0KTYTiygK187rfMo')
+
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id, '*Приветствую тебя!*', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['croissant'])
+def croissant_handler(message):
+    bot.send_message(message.chat.id,
+                     'Рецепт круассана>>> [Идеальные круассаны](https://eda.ru/recepty/vypechka-deserty/idealnye-kruassany-138570)', parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['muffin'])
+def muffin_handler(message):
+    bot.send_message(message.chat.id,
+                     'Рецепт маффинов>>> [Шоколадные маффины с кусочками шоколада](https://eda.ru/recepty/vypechka-deserty/shokoladnie-maffini-s-kusochkami-shokolada-36024)', parse_mode='Markdown')
+
+
+bot.infinity_polling()
